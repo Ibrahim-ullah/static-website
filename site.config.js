12c501e5cf7fa32e85bdfa046fb99abdfbf4e4c0
Ibrahim Ullah <34267927+Ibrahim-ullah@users.noreply.github.com>
2024-02-11 00:44:57 +0600

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Ibrahim Ullah",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/profile.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Researcher | IT Professional",
     bio: "Stay Hungry, Stay foolish",
     email: "ibrahim.ullaah@gmail.com",
