9d31de08f7e6b135cbdbee40bed304d78a908c4b
Ibrahim Ullah <34267927+Ibrahim-ullah@users.noreply.github.com>
2024-02-10 23:49:55 +0600

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,32 +1,42 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Ibrahim Ullah",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Researcher | IT Professional",
+    bio: "Stay Hungry, Stay foolish",
+    email: "ibrahim.ullaah@gmail.com",
+    linkedin: "iuemon",
+    GScholar:"citations?user=OdWHsKoAAAAJ&hl=en",
+    Researchgate: "Ibrahim-Emon",
+    github: "Ibrahim-ullah",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `FDTD using MATLAB`,
+      href: "https://github.com/Ibrahim-ullah/FDTD-method-for-electromagnatics-with-matlab-by-Atef-Z-Elsherbeni",
+    },
+    {
+      name: `CEM & Applications`,
+      href: "https://github.com/Ibrahim-ullah/Computational-Electromagnetics-and-Applications",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Md. Ibrahim Ullah",
+    description: "welcome to My Blog!",
+    theme: "light", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://ibrahim-ullah.vercel.app",
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  seo: {
+    keywords: ["Blog", "Website", "Notion"],
+  },
 
   // notion configuration (required)
   notionConfig: {
@@ -35,7 +45,7 @@ const CONFIG = {
 
   // plugin configuration (optional)
   googleAnalytics: {
-    enable: false,
+    enable: true,
     config: {
       measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
     },
@@ -43,19 +53,13 @@ const CONFIG = {
   googleSearchConsole: {
     enable: false,
     config: {
-      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
-    },
-  },
-  naverSearchAdvisor: {
-    enable: false,
-    config: {
-      siteVerification: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION || "",
+      siteVerification: process.env.GOOGLE_SITE_VERIFICATION || "",
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: "Ibrahim-ullah/static-website",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
@@ -68,7 +72,6 @@ const CONFIG = {
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  revalidateTime: 21600 * 7, // revalidate time for [slug], index
 }
 
 module.exports = { CONFIG }
